956de31f85b6ec68ca8d74e8204c4796ebce5bbe
Aleksi Hynönen <rolexpost@gmail.com>
2022-03-25 16:26:50 +0200

    Update api.test.js

--- backend/api.test.js
+++ backend/api.test.js
@@ -10,13 +10,13 @@ app.use("/", router);
 request(app)
   .get("/startingdata")
   .expect(200)
-  .expect("Data already exst")
+  .expect("Data already exist")
   .end(function (err, res) {
     try {
       if (err) throw err;
-      console.log(chalk.bgGreen.black("/startingdata", "success"));
+      console.log(chalk.bgGreen.black("GET /startingdata", "success"));
     } catch (e) {
-      console.log(chalk.bgRed.black("/startingdata", "error"));
+      console.log(chalk.bgRed.black("GET /startingdata", "error"));
     }
   });
 
@@ -27,9 +27,9 @@ request(app)
   .end(function (err, res) {
     try {
       if (err) throw err;
-      console.log(chalk.bgGreen.black("/tests", "success"));
+      console.log(chalk.bgGreen.black("GET /tests", "success"));
     } catch (e) {
-      console.log(chalk.bgRed.black("/tests", "error"));
+      console.log(chalk.bgRed.black("GET /tests", "error"));
     }
   });
 
@@ -40,9 +40,9 @@ request(app)
   .end(function (err, res) {
     try {
       if (err) throw err;
-      console.log(chalk.bgGreen.black("/tests/1", "success"));
+      console.log(chalk.bgGreen.black("GET /tests/1", "success"));
     } catch (e) {
-      console.log(chalk.bgRed.black("/tests/1", "error"));
+      console.log(chalk.bgRed.black("GET /tests/1", "error"));
     }
   });
 
@@ -52,9 +52,9 @@ request(app)
   .end(function (err, res) {
     try {
       if (err) throw err;
-      console.log(chalk.bgGreen.black("/tets", "success"));
+      console.log(chalk.bgGreen.black("GET /tets", "success"));
     } catch (e) {
-      console.log(chalk.bgRed.black("/tests", "error"));
+      console.log(chalk.bgRed.black("GET /tets", "error"));
     }
   });
 
@@ -64,8 +64,40 @@ request(app)
   .end(function (err, res) {
     try {
       if (err) throw err;
-      console.log(chalk.bgGreen.black("/tests/asd", "success"));
+      console.log(chalk.bgGreen.black("GET /tests/asd", "success"));
     } catch (e) {
-      console.log(chalk.bgRed.black("/tests/asd", "error"));
+      console.log(chalk.bgRed.black("GET /tests/asd", "error"));
+    }
+  });
+
+request(app)
+  .get("/tests/99999999")
+  .expect(204)
+  .end(function (err, res) {
+    try {
+      if (err) throw err;
+      console.log(chalk.bgGreen.black("GET /tests/99999999", "success"));
+    } catch (e) {
+      console.log(chalk.bgRed.black("GET /tests/99999999", "error"));
+    }
+  });
+
+request(app)
+  .post("/tests")
+  .send({
+    name: "Test molecyle",
+    code: "TST",
+    unit: "g/l",
+    lowerReference: 0,
+    upperReference: 10,
+  })
+  .expect("Content-Type", /json/)
+  .expect(201)
+  .end(function (err, res) {
+    try {
+      if (err) throw err;
+      console.log(chalk.bgGreen.black("POST /tests", "success"));
+    } catch (e) {
+      console.log(chalk.bgRed.black("POST /tests", "error"));
     }
   });
