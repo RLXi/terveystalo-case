20a62feb2f2c16daae1c7f99f30c7ede518b678b
Aleksi Hynönen <rolexpost@gmail.com>
2022-03-27 00:55:53 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import { router } from "./routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use("/", router);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api", router);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
