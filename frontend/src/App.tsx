3875b9af68b7f92f2c3ef2716861b7c56781805a
Aleksi Hynönen <rolexpost@gmail.com>
2022-03-27 13:04:35 +0300

    styling header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Center } from "@mantine/core";
+import { AppShell, Center, Header as MantineHeader } from "@mantine/core";
 import { Header } from "./components";
 import { Outlet } from "react-router";
 
@@ -6,7 +6,14 @@ import styles from "./App.module.css";
 
 function App() {
   return (
-    <AppShell header={<Header />} padding="md">
+    <AppShell
+      header={
+        <MantineHeader height={60} p={"md"}>
+          <Header />
+        </MantineHeader>
+      }
+      padding="md"
+    >
       <Center className={styles.App}>
         <Outlet />
       </Center>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Center, Group } from "@mantine/core";
+import { Button, Center, Group } from "@mantine/core";
 
 import { Link } from "react-router-dom";
 
@@ -7,9 +7,15 @@ export function Header() {
     <div>
       <Center>
         <Group>
-          <Link to="/">Listing</Link>
-          <Link to="/create">Create</Link>
-          <Link to="/about">About</Link>
+          <Link to="/">
+            <Button variant="subtle">Listing</Button>
+          </Link>
+          <Link to="/create">
+            <Button variant="subtle">Create</Button>
+          </Link>
+          <Link to="/about">
+            <Button variant="subtle">About</Button>
+          </Link>
         </Group>
       </Center>
     </div>
